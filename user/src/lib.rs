f3a00f7e746e9ca2eda0fe4da8116513d3ef2e5e
StevenDUANN <fanchuduan@gmail.com>
2023-12-28 23:05:42 +0800

    21301005dcf Experiment6 first commitment

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -9,20 +9,9 @@ mod lang_items;
 
 use syscall::*;
 
-fn clear_bss() {
-    extern "C" {
-        fn start_bss();
-        fn end_bss();
-    }
-    (start_bss as usize..end_bss as usize).for_each(|addr| {
-        unsafe { (addr as *mut u8).write_volatile(0); }
-    });
-}
-
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
-    clear_bss();
     exit(main());
     panic!("unreachable after sys_exit!");
 }
